c5391184d6e915ccbd365b203dcd9f4bb0960a90
ChalozMe <ccarpiop@unsa.edu.pe>
2026-05-03 18:17:41 -0500

    Extract with pandas, made a jupiternotebook to study data

---
+++ src/extract.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def extract():
+    catalog = pd.read_csv(os.path.join(BASE_DIR, "data/raw/Catalog_Orders.txt"))
+    web = pd.read_csv(os.path.join(BASE_DIR, "data/raw/Web_orders.txt"))
+    products = pd.read_csv(os.path.join(BASE_DIR, "data/raw/products.txt"))
+
+    return catalog, web, products
+
+
+if __name__ == "__main__":
+    c, w, p = extract()
+
+    print("Catalog:")
+    print(c.head())
+
+    print("\nWeb:")
+    print(w.head())
+
+    print("\nProducts:")
+    print(p.head())
