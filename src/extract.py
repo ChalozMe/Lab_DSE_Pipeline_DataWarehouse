bf68d3b1b4fedd8fe2c79c92ce4c57ff839d7f30
doomed-phobos <the_ekj.2121@outlook.com>
2026-05-10 18:58:41 -0500

    fix: better extraction for web orders

--- src/extract.py
+++ src/extract.py
@@ -6,7 +6,23 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def extract():
     catalog = pd.read_csv(os.path.join(BASE_DIR, "data/raw/Catalog_Orders.txt"))
-    web = pd.read_csv(os.path.join(BASE_DIR, "data/raw/Web_orders.txt"))
+
+    web = pd.read_csv(
+    os.path.join(BASE_DIR, "data/raw/Web_orders.txt"),
+    sep=";",
+    header=0,
+    names=[
+        "ID",
+        "INV",
+        "PCODE",
+        "DATE",
+        "CATALOG",
+        "QTY",
+        "CUSTOMER"
+    ],
+    quotechar='"'
+)
+
     products = pd.read_csv(os.path.join(BASE_DIR, "data/raw/products.txt"))
 
     return catalog, web, products
